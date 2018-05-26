a08dad6855a06773c4cb6fe9b0033a37d0fe0c78
unknown <bryan.knight@live.com>
2018-05-25 20:11:44 -0400

    Finishing Try it Yourself 3-6

--- Learning/Lessons/Chapter 3/lists2_try_it_yourself.py
+++ Learning/Lessons/Chapter 3/lists2_try_it_yourself.py
@@ -10,8 +10,23 @@ invitations.remove(cancelled)
 print(invitations)
 print("\nThe guest " + cancelled.title() + " can no longer make it and has cancelled.")
 
-invitations.insert(0, 'Ghandi')
+invitations.insert(0, 'ghandi')
 print(invitations)
 
 print("You are invited to a party! Here is the current list " + invitations[0].title() + ', ' + invitations[1].title() + ', ' + invitations [2].title() + '.')
 print( "I have found a bigger table, We will be inviting more people. Who should I invite? ")
+
+invitations.insert(0, 'john doe')
+invitations.insert(2, 'binx bah')
+invitations.insert(5, 'salem bah')
+invitations.append('maigy moo')
+print(invitations)
+
+invite = "You've been invited! "
+print(invite + invitations[0])
+print(invite + invitations[1])
+print(invite + invitations[2])
+print(invite + invitations[3])
+print(invite + invitations[4])
+print(invite + invitations[5])
+print(invite + invitations[6])
--- Learning/Lessons/Chapter 3/pop.py
+++ Learning/Lessons/Chapter 3/pop.py
@@ -39,5 +39,3 @@ too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
 print(motorcycles)
 print("\nA " + too_expensive.title() + " is too expensive for me")
-
-Test commit
