a382f960d6372efb507ab716317af1fbfdc5fff2
unknown <bryan.knight@live.com>
2018-05-29 18:05:27 -0400

    Completed 4.2

--- Learning/Lessons/Chapter 4/C4_4.1_to_4.2.py
+++ Learning/Lessons/Chapter 4/C4_4.1_to_4.2.py
@@ -10,3 +10,14 @@ for favorite_pizza in favorite_pizzas:
     print("I love "+ favorite_pizza.title()+ " pizza!")
 
 print("\nI love all kinds of pizza!")
+
+animals = ['cat', 'dog', 'bird']
+for animal in animals:
+    print(animal)
+
+print("\n")
+
+for animal in animals:
+    print("I think the follow animals are cool: "+ animal.title())
+
+print("\nAny of these animals would make a great pet!")
