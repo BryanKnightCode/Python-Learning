d47d45327701efd9b5345170c657e746b439c8f2
Harm <Bryan.knight@live.com>
2018-05-29 16:44:59 -0400

    chapter 4

---
+++ Learning/Lessons/Chapter 4/C4_4.1_to_4.2.py
@@ -0,0 +1,12 @@
+# Print the names of all the pizzas.
+favorite_pizzas = ['pepperoni', 'meat lovers', 'pineapple']
+for favorite_pizza in favorite_pizzas:
+    print(favorite_pizza)
+
+print("\n")
+
+ # Print a sentence aboout each pizza.
+for favorite_pizza in favorite_pizzas:
+    print("I love "+ favorite_pizza.title()+ " pizza!")
+
+print("\nI love all kinds of pizza!")
